08059c535b2eb716ac0169c3c96b31092f64e804
gabyechegarays <32307573+gabyechegarays@users.noreply.github.com>
2023-05-03 17:20:59 -0700

    Update and rename PolizaVencidas.tsx to Chart.tsx

---
+++ src/Components/Chart.tsx
@@ -0,0 +1,12 @@
+import { Chart } from "./Components/Chart";
+import { data } from "./Components/Data";
+import { data2 } from "./Components/Data1";
+
+export function App() {
+  return (
+    <div>
+      <Chart title={"Poliza Vencidas"} data={data} />
+     <Chart title={"Poliza Vendidas"} data={data2} />
+    </div>
+  );
+}
